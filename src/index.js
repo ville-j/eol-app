9405a440f67a84baf04aee4a7157679f3ff9d2b8
Ville Jänkälä <ville.jankala@gmail.com>
2021-01-19 22:13:46 +0200

    pwa stuff

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import * as serviceWorker from "./serviceWorker";
 import store from "./store";
 
 const theme = createMuiTheme({
@@ -48,8 +48,8 @@ const render = () => {
 };
 
 render();
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
